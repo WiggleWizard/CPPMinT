96422869ccd28eaad29fa2da5fdae565c6a69201
WiggleWizard <1405402+WiggleWizard@users.noreply.github.com>
2022-08-20 18:07:52 +0100

    Main.cpp formatting

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
 {
     SimpleBench bench(true);
 
-	const eastl::string str("Hello World");
-	spdlog::info("{}", str.c_str());
+    const eastl::string str("Hello World");
+    spdlog::info("{}", str.c_str());
 
     bench.Stop();
     spdlog::info("Took {}us to execute", bench.GetDurationInUs());
